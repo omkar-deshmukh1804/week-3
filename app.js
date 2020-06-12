a93ffde0190e6c0ba8c523083bcc5e68b36299f1
omkar-deshmukh1804 <omidesmhukh11@gmil.com>
2020-06-12 21:21:55 +0530

    Second Update Adding and Deleting Todos

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
-
-var ComponentA = {
-    props: ['headertitle', 'todos'],
-    template:`<div class="card">
-    <div class="card-body">
-        <h4 class="card-title" >
-            {{headertitle}}<button type="button" class="btn" data-target="#addtodoModal" data-toggle="collapse"><span class="fa fa-plus fa-xs"></span></button></h4>       
-    </div> 
-    <div class="collapse" id="addtodoModal">
-    </div>
-</div>`
-}
 var ComponentB = {
-    components: {
-        'component-a': ComponentB
-      },
     props:['todos'],
     template:`<div class="todoitems">
-                <li class="list-group-item" >
-                    <a type="button" href="#">{{todos.todoTitle}}</a>
-                    <button type="button" class="btn"><i class="fa fa-trash"></i></button>
-                </li>   
-            </div>`
+                    <li class="list-group-item" >
+                        <a href="#">{{todos.todoTitle}}</a>
+                        <button type="button" class="btn" @click="deleteTodo(todos.id)">
+                            <i class="fa fa-trash" ></i>
+                        </button>
+                    </li>
+            </div>`,
+    methods: {
+        deleteTodo (id) {
+            // this.todos = this.todos.filter(todo => todo.id !== id);
+            axios
+              .delete(`http://localhost:3000/todos/${id}`)
+              .then(res => this.todos=res.data)
+              .catch(err => console.error(err));
+        }
+    },
     
 }
 
+var ComponentA = {
+    
+    data: function () {
+        return {
+            showAddNewTodo: false,
+            newTodoTitle:'',
 
+        }
+    },
+    props: ['headertitle', 'todos'],
+    components: {
+         'component-b' : ComponentB,
+      },
+    
+    template:`<div class="card">
+                <div class="card-body">
+                    <h4 class="card-title" >
+                        {{headertitle}}<button type="button" class="btn" @click="toggleAdd" ><span class="fa fa-plus fa-xs"></span>
+                        </button>
+                    </h4>       
+                    <form v-show="showAddNewTodo" @submit="">
+                        <input type= "text" placeholder="Add new todo"  v-model="newTodoTitle">
+                        <button type="submit" class="btn btn-warning btn-sm" @click="addTodos(newTodoTitle)" >Add Todo</button>
+                    </form>
+                        <component-b v-for="todo in todos" :key="todo.id" :todos="todo"></component-b>
+                </div>
+</div>`,
+    
+    methods: {
+        toggleAdd() {
+            this.showAddNewTodo = !this.showAddNewTodo;
+        },
+        addTodos(newTodoTitle)
+        {
+            axios
+                .post('http://localhost:3000/todos',{
+                  todoTitle : newTodoTitle
+              })
+              .then(res => this.todos=res.data)
+              .catch(err => console.error(err));
+        }
+        
+    },
+}
 
 
 
@@ -37,8 +76,12 @@ new Vue({
         'component-b': ComponentB
       },
     data: {
-        todos: [],
-        headertitle: "To do lists",
+        
+            todos: [],
+            headertitle: "To do lists",
+            showAddNewTodo : false,
+        
+        
     },
     mounted() {
         axios
@@ -46,6 +89,10 @@ new Vue({
           .then(res => this.todos = res.data )
             .catch(err => console.error(err));
         
+    },
+    methods: {
+       
+       
     },
    
    
